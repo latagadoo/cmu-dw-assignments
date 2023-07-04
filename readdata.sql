d98867685a1097aa79c7693af392e4a3b4d06679
Lata Gadoo <lata_gadoo@hotmail.com>
2023-07-04 11:22:05 -0500

    updated readdata.sql that ingests data for tables

--- readdata.sql
+++ readdata.sql
@@ -1,10 +1,18 @@
-CREATE TABLE  bike_data AS SELECT * FROM read_csv_auto('.\project\data\bike\*.csv.gz', union_by_name=True , all_varchar=1);
-CREATE TABLE fhv_tripdata AS SELECT * FROM read_parquet('.\project\data\taxi\fhv_tripdata_*.parquet');
-CREATE TABLE fhvhv_tripdata AS SELECT * FROM read_parquet('.\project\data\taxi\fhvhv_tripdata_*.parquet');
-CREATE TABLE yellow_tripdata AS SELECT * FROM read_parquet('.\project\data\taxi\yellow_tripdata_*.parquet');
-CREATE TABLE green_tripdata AS SELECT * FROM read_parquet('.\project\data\taxi\green_tripdata_*.parquet');
-CREATE TABLE central_park_weather AS SELECT * FROM '.\project\data\central_park_weather.csv';
-CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM '.\project\data\daily_citi_bike_trip_counts_and_weather.csv';
-CREATE TABLE fhv_bases AS SELECT * FROM '.\project\data\fhv_bases.csv';
+CREATE TABLE  bike_data AS SELECT * FROM read_csv_auto('.\project\data\bike\*.csv.gz', union_by_name=True , all_varchar=1 , filename=1);
+CREATE TABLE fhv_tripdata AS SELECT * FROM read_parquet('.\project\data\taxi\fhv_tripdata_*.parquet',filename=1);
+CREATE TABLE fhvhv_tripdata AS SELECT * FROM read_parquet('.\project\data\taxi\fhvhv_tripdata_*.parquet',filename=1);
+CREATE TABLE yellow_tripdata AS SELECT * FROM read_parquet('.\project\data\taxi\yellow_tripdata_*.parquet',filename=1);
+CREATE TABLE green_tripdata AS SELECT * FROM read_parquet('.\project\data\taxi\green_tripdata_*.parquet', filename=1);
+CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('.\project\data\central_park_weather.csv',all_varchar=1 , filename=1);
+CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM read_csv_auto('.\project\data\daily_citi_bike_trip_counts_and_weather.csv',all_varchar=1 , filename=1);
+CREATE TABLE fhv_bases AS SELECT * FROM read_csv_auto('.\project\data\fhv_bases.csv');
+ALTER TABLE fhv_bases
+RENAME COLUMN column0 to base_number;
+ALTER TABLE fhv_bases
+RENAME COLUMN column1 to base_name;
+ALTER TABLE fhv_bases
+RENAME COLUMN column2 to dba;
+ALTER TABLE fhv_bases
+RENAME COLUMN column3 to dba_category;
 
 
